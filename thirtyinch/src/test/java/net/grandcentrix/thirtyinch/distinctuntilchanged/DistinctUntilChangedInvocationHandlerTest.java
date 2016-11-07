be6dcab22ded041ff348f02a7bc8c399be6d43bb
Pascal Welsch <pascal.welsch@grandcentrix.net>
2016-11-07 20:04:18 +0100

    Upgrade to latest assertJ

--- thirtyinch/src/test/java/net/grandcentrix/thirtyinch/distinctuntilchanged/DistinctUntilChangedInvocationHandlerTest.java
+++ thirtyinch/src/test/java/net/grandcentrix/thirtyinch/distinctuntilchanged/DistinctUntilChangedInvocationHandlerTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 
 import java.lang.reflect.Method;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
