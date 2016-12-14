0ce42d11c24a8e914cd17393e00e57f078e500dc
StefMa <StefMaDev@outlook.com>
2016-12-14 11:30:37 +0100

    Rename tests

--- rx/src/test/java/net/grandcentrix/thirtyinch/rx/RxTiPresenterSubscriptionHandlerTest.java
+++ rx/src/test/java/net/grandcentrix/thirtyinch/rx/RxTiPresenterSubscriptionHandlerTest.java
@@ -3,7 +3,6 @@ package net.grandcentrix.thirtyinch.rx;
 import net.grandcentrix.thirtyinch.TiView;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,10 +10,8 @@ import org.junit.runners.JUnit4;
 
 import rx.observers.TestSubscriber;
 
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.hamcrest.core.StringContains.containsString;
 import static org.mockito.Mockito.mock;
 
 @RunWith(JUnit4.class)
@@ -40,8 +37,52 @@ public class RxTiPresenterSubscriptionHandlerTest {
         mSubscriptionHandler = null;
     }
 
+    @Test(expected = AssertionError.class)
+    public void tesManageViewSubscription_DetachBeforeAttach_ShouldThrowAssertError()
+            throws Exception {
+        mPresenter.create();
+        TestSubscriber<Integer> testSubscriber = new TestSubscriber<>();
+
+        mSubscriptionHandler.manageViewSubscription(testSubscriber);
+        mPresenter.detachView();
+
+        testSubscriber.assertUnsubscribed();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testManageSubscription_AfterDestroy_ShouldThrowIllegalState() throws Exception {
+        mPresenter.create();
+        mPresenter.destroy();
+        TestSubscriber<Integer> testSubscriber = new TestSubscriber<>();
+
+        mSubscriptionHandler.manageSubscription(testSubscriber);
+    }
+
     @Test
-    public void testDetach() throws Exception {
+    public void testManageSubscription_WithAlreadyUnsubscribedSubscription_ShouldDoNothing()
+            throws Exception {
+        TestSubscriber<Integer> testSubscriber = new TestSubscriber<>();
+        testSubscriber.unsubscribe();
+
+        mSubscriptionHandler.manageSubscription(testSubscriber);
+
+        testSubscriber.assertUnsubscribed();
+    }
+
+    @Test
+    public void testManageSubscription_WithDestroy_ShouldUnsubscribe() throws Exception {
+        mPresenter.create();
+        TestSubscriber<Integer> testSubscriber = new TestSubscriber<>();
+
+        mSubscriptionHandler.manageSubscription(testSubscriber);
+        assertThat(testSubscriber.isUnsubscribed(), equalTo(false));
+
+        mPresenter.destroy();
+        testSubscriber.assertUnsubscribed();
+    }
+
+    @Test
+    public void testManageViewSubscription_WithDetachView_ShouldUnsubscribe() throws Exception {
         mPresenter.create();
         mPresenter.attachView(mView);
         TestSubscriber<Integer> testSubscriber = new TestSubscriber<>();
@@ -50,70 +91,18 @@ public class RxTiPresenterSubscriptionHandlerTest {
         mPresenter.detachView();
 
         testSubscriber.assertUnsubscribed();
-        assertThat(mPresenter.getView(), nullValue());
-        assertThat(mPresenter.onDetachCalled, equalTo(1));
     }
 
     @Test
-    public void testDetachBeforeAttach() throws Exception {
-        mPresenter.create();
-        TestSubscriber<Integer> testSubscriber = new TestSubscriber<>();
-
-        mSubscriptionHandler.manageViewSubscription(testSubscriber);
-        mPresenter.detachView();
-
-        assertThat(testSubscriber.isUnsubscribed(), equalTo(false));
-        assertThat(mPresenter.onDetachCalled, equalTo(0));
-    }
-
-    @Test
-    public void testManageSubscription() throws Exception {
-        mPresenter.create();
-        TestSubscriber<Integer> testSubscriber = new TestSubscriber<>();
-
-        mSubscriptionHandler.manageSubscription(testSubscriber);
-
-        assertThat(testSubscriber.isUnsubscribed(), equalTo(false));
-
-        mPresenter.destroy();
-
-        testSubscriber.assertUnsubscribed();
-    }
-
-    @Test
-    public void testManageSubscriptionDestroyed() throws Exception {
-        mPresenter.create();
-        mPresenter.destroy();
-        TestSubscriber<Integer> testSubscriber = new TestSubscriber<>();
-
-        try {
-            mSubscriptionHandler.manageSubscription(testSubscriber);
-            Assert.fail("no exception");
-        } catch (IllegalStateException e) {
-            assertThat(e.getMessage(), containsString("DESTROYED"));
-        }
-    }
-
-    @Test
-    public void testManageSubscriptionUnsubscribed() throws Exception {
-        TestSubscriber<Integer> testSubscriber = new TestSubscriber<>();
-        testSubscriber.unsubscribe();
-        mSubscriptionHandler.manageSubscription(testSubscriber);
-        testSubscriber.assertUnsubscribed();
-    }
-
-    @Test
-    public void testManageViewSubscription() throws Exception {
+    public void testManageViewSubscription_WithDetach_ShouldUnsubscribe() throws Exception {
         mPresenter.create();
         mPresenter.attachView(mock(TiView.class));
         TestSubscriber<Integer> testSubscriber = new TestSubscriber<>();
 
         mSubscriptionHandler.manageViewSubscription(testSubscriber);
-
         assertThat(testSubscriber.isUnsubscribed(), equalTo(false));
 
         mPresenter.detachView();
-
         testSubscriber.assertUnsubscribed();
     }
 
