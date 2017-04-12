f4987be5afae9cc5b163a3d99613bf89467cb700
Pascal Welsch <pascal.welsch@grandcentrix.net>
2017-04-12 22:23:03 +0200

    Log view invocations in sample

--- sample/src/main/java/net/grandcentrix/thirtyinch/sample/HelloWorldActivity.java
+++ sample/src/main/java/net/grandcentrix/thirtyinch/sample/HelloWorldActivity.java
@@ -18,6 +18,7 @@ package net.grandcentrix.thirtyinch.sample;
 
 import com.jakewharton.rxbinding.view.RxView;
 
+import net.grandcentrix.thirtyinch.LoggingInterceptor;
 import net.grandcentrix.thirtyinch.TiActivity;
 import net.grandcentrix.thirtyinch.sample.fragmentlifecycle.FragmentLifecycleActivity;
 import net.grandcentrix.thirtyinch.sample.fragmentlifecycle.viewpager.LifecycleViewPagerActivity;
@@ -41,6 +42,10 @@ public class HelloWorldActivity extends TiActivity<HelloWorldPresenter, HelloWor
     private TextView mOutput;
 
     private TextView mUptime;
+
+    public HelloWorldActivity() {
+        addBindViewInterceptor(new LoggingInterceptor());
+    }
 
     @Override
     public Observable<Void> onButtonClicked() {
