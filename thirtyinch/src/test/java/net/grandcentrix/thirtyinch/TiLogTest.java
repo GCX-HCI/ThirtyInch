a10d3af9fb979c47d3c6bea71d49961bf0e3e8c4
Pascal Welsch <pascal.welsch@grandcentrix.net>
2017-04-13 11:54:36 +0200

    Test TiLog

--- thirtyinch/src/test/java/net/grandcentrix/thirtyinch/TiLogTest.java
+++ thirtyinch/src/test/java/net/grandcentrix/thirtyinch/TiLogTest.java
@@ -16,11 +16,139 @@
 package net.grandcentrix.thirtyinch;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import android.util.Log;
 
 import static junit.framework.Assert.fail;
 import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class TiLogTest {
+
+    @Test
+    public void dontCrashForNullLogger() throws Exception {
+        TiLog.setLogger(null);
+        TiLog.v("tag", "msg");
+        TiLog.i("tag", "msg");
+        TiLog.d("tag", "msg");
+        TiLog.e("tag", "msg");
+        TiLog.w("tag", "msg");
+        TiLog.log(Log.VERBOSE, "tag", "msg");
+    }
+
+    @Test
+    public void logDToLogger() throws Exception {
+        final TiLog.Logger logger = mock(TiLog.Logger.class);
+        TiLog.setLogger(logger);
+        final ArgumentCaptor<Integer> levelCaptor = ArgumentCaptor.forClass(Integer.class);
+        final ArgumentCaptor<String> tagCaptor = ArgumentCaptor.forClass(String.class);
+        final ArgumentCaptor<String> msgCaptor = ArgumentCaptor.forClass(String.class);
+
+        TiLog.d("tag", "msg");
+        verify(logger).log(levelCaptor.capture(), tagCaptor.capture(), msgCaptor.capture());
+
+        assertThat(levelCaptor.getValue()).isEqualTo(Log.DEBUG);
+        assertThat(tagCaptor.getValue()).isEqualTo("tag");
+        assertThat(msgCaptor.getValue()).isEqualTo("msg");
+    }
+
+    @Test
+    public void logEToLogger() throws Exception {
+        final TiLog.Logger logger = mock(TiLog.Logger.class);
+        TiLog.setLogger(logger);
+        final ArgumentCaptor<Integer> levelCaptor = ArgumentCaptor.forClass(Integer.class);
+        final ArgumentCaptor<String> tagCaptor = ArgumentCaptor.forClass(String.class);
+        final ArgumentCaptor<String> msgCaptor = ArgumentCaptor.forClass(String.class);
+
+        TiLog.e("tag", "msg");
+        verify(logger).log(levelCaptor.capture(), tagCaptor.capture(), msgCaptor.capture());
+
+        assertThat(levelCaptor.getValue()).isEqualTo(Log.ERROR);
+        assertThat(tagCaptor.getValue()).isEqualTo("tag");
+        assertThat(msgCaptor.getValue()).isEqualTo("msg");
+    }
+
+    @Test
+    public void logIToLogger() throws Exception {
+        final TiLog.Logger logger = mock(TiLog.Logger.class);
+        TiLog.setLogger(logger);
+        final ArgumentCaptor<Integer> levelCaptor = ArgumentCaptor.forClass(Integer.class);
+        final ArgumentCaptor<String> tagCaptor = ArgumentCaptor.forClass(String.class);
+        final ArgumentCaptor<String> msgCaptor = ArgumentCaptor.forClass(String.class);
+
+        TiLog.i("tag", "msg");
+        verify(logger).log(levelCaptor.capture(), tagCaptor.capture(), msgCaptor.capture());
+
+        assertThat(levelCaptor.getValue()).isEqualTo(Log.INFO);
+        assertThat(tagCaptor.getValue()).isEqualTo("tag");
+        assertThat(msgCaptor.getValue()).isEqualTo("msg");
+    }
+
+    @Test
+    public void logVToLogger() throws Exception {
+        final TiLog.Logger logger = mock(TiLog.Logger.class);
+        TiLog.setLogger(logger);
+        final ArgumentCaptor<Integer> levelCaptor = ArgumentCaptor.forClass(Integer.class);
+        final ArgumentCaptor<String> tagCaptor = ArgumentCaptor.forClass(String.class);
+        final ArgumentCaptor<String> msgCaptor = ArgumentCaptor.forClass(String.class);
+
+        TiLog.v("tag", "msg");
+        verify(logger).log(levelCaptor.capture(), tagCaptor.capture(), msgCaptor.capture());
+
+        assertThat(levelCaptor.getValue()).isEqualTo(Log.VERBOSE);
+        assertThat(tagCaptor.getValue()).isEqualTo("tag");
+        assertThat(msgCaptor.getValue()).isEqualTo("msg");
+    }
+
+    @Test
+    public void logWToLogger() throws Exception {
+        final TiLog.Logger logger = mock(TiLog.Logger.class);
+        TiLog.setLogger(logger);
+        final ArgumentCaptor<Integer> levelCaptor = ArgumentCaptor.forClass(Integer.class);
+        final ArgumentCaptor<String> tagCaptor = ArgumentCaptor.forClass(String.class);
+        final ArgumentCaptor<String> msgCaptor = ArgumentCaptor.forClass(String.class);
+
+        TiLog.w("tag", "msg");
+        verify(logger).log(levelCaptor.capture(), tagCaptor.capture(), msgCaptor.capture());
+
+        assertThat(levelCaptor.getValue()).isEqualTo(Log.WARN);
+        assertThat(tagCaptor.getValue()).isEqualTo("tag");
+        assertThat(msgCaptor.getValue()).isEqualTo("msg");
+    }
+
+    @Test
+    public void loglogDToLogger() throws Exception {
+        final TiLog.Logger logger = mock(TiLog.Logger.class);
+        TiLog.setLogger(logger);
+        final ArgumentCaptor<Integer> levelCaptor = ArgumentCaptor.forClass(Integer.class);
+        final ArgumentCaptor<String> tagCaptor = ArgumentCaptor.forClass(String.class);
+        final ArgumentCaptor<String> msgCaptor = ArgumentCaptor.forClass(String.class);
+
+        TiLog.log(Log.DEBUG, "tag", "msg");
+        verify(logger).log(levelCaptor.capture(), tagCaptor.capture(), msgCaptor.capture());
+
+        assertThat(levelCaptor.getValue()).isEqualTo(Log.DEBUG);
+        assertThat(tagCaptor.getValue()).isEqualTo("tag");
+        assertThat(msgCaptor.getValue()).isEqualTo("msg");
+    }
+
+    @Test
+    public void loglogVToLogger() throws Exception {
+        final TiLog.Logger logger = mock(TiLog.Logger.class);
+        TiLog.setLogger(logger);
+        final ArgumentCaptor<Integer> levelCaptor = ArgumentCaptor.forClass(Integer.class);
+        final ArgumentCaptor<String> tagCaptor = ArgumentCaptor.forClass(String.class);
+        final ArgumentCaptor<String> msgCaptor = ArgumentCaptor.forClass(String.class);
+
+        TiLog.log(Log.VERBOSE, "tag", "msg");
+        verify(logger).log(levelCaptor.capture(), tagCaptor.capture(), msgCaptor.capture());
+
+        assertThat(levelCaptor.getValue()).isEqualTo(Log.VERBOSE);
+        assertThat(tagCaptor.getValue()).isEqualTo("tag");
+        assertThat(msgCaptor.getValue()).isEqualTo("msg");
+    }
 
     @Test
     public void preventSettingRecursiveLogger() throws Exception {
