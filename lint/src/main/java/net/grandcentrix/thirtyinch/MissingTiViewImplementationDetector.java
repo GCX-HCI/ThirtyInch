abf377a7a73c20bef2f5135e04a3b3c5dc27ebc2
Marcel Schnelle <aurae@web.de>
2017-07-23 11:20:38 +0900

    First iteration of a Lint Detector for TiActivity/TiFragment sub-classes

--- lint/src/main/java/net/grandcentrix/thirtyinch/MissingTiViewImplementationDetector.java
+++ lint/src/main/java/net/grandcentrix/thirtyinch/MissingTiViewImplementationDetector.java
@@ -7,40 +7,151 @@ import com.android.tools.lint.detector.api.Issue;
 import com.android.tools.lint.detector.api.JavaContext;
 import com.android.tools.lint.detector.api.Scope;
 import com.android.tools.lint.detector.api.Severity;
+import com.android.tools.lint.detector.api.TextFormat;
+import com.intellij.psi.JavaElementVisitor;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiClassType;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiType;
+import com.intellij.psi.PsiTypeParameter;
+import com.intellij.psi.util.PsiUtil;
 
-import org.jetbrains.uast.UClass;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
 
-public final class MissingTiViewImplementationDetector extends Detector implements Detector.UastScanner {
+// TODO Check for CompositeActivity
+@SuppressWarnings("WeakerAccess")
+public final class MissingTiViewImplementationDetector extends Detector implements Detector.JavaPsiScanner {
 
     static final Issue ISSUE = Issue.create(
             "MissingTiViewImplementation",
             "TiView Implementation missing in class",
-            "With ThirtyInch, an Activity extending TiActivity or CompositeActivity has to implement the View interface in its signature.",
+            "When using ThirtyInch, a class extending TiActivity, TiFragment or CompositeActivity " +
+                    "has to implement the TiView interface associated with it in its signature, " +
+                    "or implement `provideView()` instead to override this default behaviour.",
             Category.CORRECTNESS,
             8,
             Severity.ERROR,
             new Implementation(MissingTiViewImplementationDetector.class, EnumSet.of(Scope.JAVA_FILE, Scope.TEST_SOURCES)));
 
     @Override
-    public List<String> applicableSuperClasses() {
-        List<String> superClasses = new ArrayList<>();
-        superClasses.add("net.grandcentrix.thirtyinch.TiActivity");
-        superClasses.add("com.pascalwelsch.compositeandroid.activity.CompositeActivity");
-        return superClasses;
+    public List<Class<? extends PsiElement>> getApplicablePsiTypes() {
+        return Collections.<Class<? extends PsiElement>>singletonList(PsiClass.class);
     }
 
     @Override
-    public void visitClass(JavaContext context, UClass declaration) {
-        super.visitClass(context, declaration);
+    public JavaElementVisitor createPsiVisitor(JavaContext context) {
+        if (context.isEnabled(ISSUE)) {
+            return new ClassVisitor(context);
+        }
 
-        context.log(null, "Visit Declaration: " + declaration.asLogString());
+        return null;
+    }
 
-        // TODO Check for Fragments
-        // TODO Check type parameters
-        // TODO Check if provideView() overridden
+    private static final class ClassVisitor extends JavaElementVisitor {
+
+        private static final String TI_VIEW_FQ = "net.grandcentrix.thirtyinch.TiView";
+        private static final String TI_ACTIVITY_FQ = "net.grandcentrix.thirtyinch.TiActivity";
+        private static final String TI_FRAGMENT_FQ = "net.grandcentrix.thirtyinch.TiFragment";
+        private static final String PROVIDE_VIEW_METHOD = "provideView";
+
+        private final JavaContext context;
+
+        ClassVisitor(JavaContext context) {
+            this.context = context;
+        }
+
+        @Override
+        public void visitClass(PsiClass declaration) {
+            super.visitClass(declaration);
+
+            // Don't trigger on abstract classes
+            if (PsiUtil.isAbstractClass(declaration)) {
+                return;
+            }
+
+            // Extract the MVP View interface type from the class
+            PsiType resolvedViewInterface = resolveRelevantViewInterface(declaration);
+            if (resolvedViewInterface == null) {
+                return;
+            }
+
+            // Check if the Activity implements that interface
+            boolean detectedView = false;
+            for (PsiClassType implementedType : declaration.getImplementsListTypes()) {
+                if (implementedType.equals(resolvedViewInterface)) {
+                    detectedView = true;
+                    break;
+                }
+            }
+
+            if (!detectedView) {
+                // Check if provideView() is overridden instead
+                boolean detectedOverride = false;
+                for (PsiMethod method : declaration.findMethodsByName(PROVIDE_VIEW_METHOD, true)) {
+                    if (resolvedViewInterface.equals(method.getReturnType())) {
+                        detectedOverride = true;
+                        break;
+                    }
+                }
+
+                if (!detectedOverride) {
+                    // Report issue for this class
+                    context.report(ISSUE, context.getLocation(declaration.getNameIdentifier()), ISSUE.getBriefDescription(TextFormat.TEXT));
+                }
+            }
+        }
+
+        @Nullable
+        private PsiType resolveRelevantViewInterface(@NotNull PsiClass declaration) {
+            for (PsiClassType extendedType : declaration.getExtendsListTypes()) {
+                PsiClass resolvedType = PsiUtil.resolveGenericsClassInType(extendedType).getElement();
+                if (resolvedType == null) {
+                    logInternalError("Unable to resolve type '" + extendedType.getClassName() + "', extended by class '" + declaration.getName() + "'");
+                    return null;
+                }
+
+                String qualifiedName = resolvedType.getQualifiedName();
+                if (TI_ACTIVITY_FQ.equals(qualifiedName) || TI_FRAGMENT_FQ.equals(qualifiedName)) {
+                    return resolveViewFromParameters(resolvedType.getTypeParameters(), extendedType.getParameters());
+                }
+            }
+
+            return null;
+        }
+
+        @Nullable
+        private PsiType resolveViewFromParameters(PsiTypeParameter[] parameterTypes, PsiType[] parameters) {
+            // Expect <P extends TiPresenter, V extends TiView>
+            if (parameters.length != 2 || parameterTypes.length != 2) {
+                return null;
+            }
+
+            // Check that the second type parameter is actually a TiView
+            PsiTypeParameter parameterType = parameterTypes[1];
+            PsiType parameter = parameters[1];
+            for (PsiClassType extendedType : parameterType.getExtendsListTypes()) {
+                PsiClass resolvedType = PsiUtil.resolveGenericsClassInType(extendedType).getElement();
+                if (resolvedType == null) {
+                    logInternalError("Unable to resolve type '" + extendedType.getClassName() + "', used as parameter");
+                    return null;
+                }
+
+                if (TI_VIEW_FQ.equals(resolvedType.getQualifiedName())) {
+                    return parameter;
+                }
+            }
+
+            return null;
+        }
+
+        private void logInternalError(String message) {
+            context.log(new IllegalStateException(message), "");
+        }
     }
 }
--- lint/src/test/java/net/grandcentrix/thirtyinch/MissingTiViewImplementationDetectorTest.java
+++ lint/src/test/java/net/grandcentrix/thirtyinch/MissingTiViewImplementationDetectorTest.java
@@ -4,8 +4,6 @@ import com.android.tools.lint.checks.infrastructure.LintDetectorTest;
 import com.android.tools.lint.detector.api.Detector;
 import com.android.tools.lint.detector.api.Issue;
 
-import org.intellij.lang.annotations.Language;
-
 import java.util.Collections;
 import java.util.List;
 
@@ -20,7 +18,11 @@ public class MissingTiViewImplementationDetectorTest extends LintDetectorTest {
     private final TestFile tiActivityStub = java("" +
             "package net.grandcentrix.thirtyinch;" +
             "public abstract class TiActivity<P extends TiPresenter<V>, V extends TiView> {" +
-            "   public abstract P providePresenter();" +
+            "}");
+
+    private final TestFile tiFragmentStub = java("" +
+            "package net.grandcentrix.thirtyinch;" +
+            "public abstract class TiFragment<P extends TiPresenter<V>, V extends TiView> {" +
             "}");
 
     private final TestFile tiPresenterStub = java("" +
@@ -33,35 +35,171 @@ public class MissingTiViewImplementationDetectorTest extends LintDetectorTest {
             "public interface TiView {" +
             "}");
 
+    private final TestFile view = java("" +
+            "package foo;" +
+            "import net.grandcentrix.thirtyinch.*;" +
+            "interface MyView extends TiView {" +
+            "}");
+
+    private final TestFile presenter = java("" +
+            "package foo;" +
+            "import net.grandcentrix.thirtyinch.*;" +
+            "final class MyPresenter extends TiPresenter<MyView> {" +
+            "}");
+
     /* Test Cases */
 
-    public void testSomething() throws Exception {
-        TestFile view = java("" +
+    public void testTiActivity_dontTriggerOnAbstractClass() throws Exception {
+        TestFile activity = java("" +
                 "package foo;" +
                 "import net.grandcentrix.thirtyinch.*;" +
-                "interface MyView extends TiView {" +
+                "public abstract class MyActivity extends TiActivity<MyPresenter, MyView> {" +
                 "}");
 
-        TestFile presenter = java("" +
+        assertThat(lintProject(
+                tiActivityStub, tiPresenterStub, tiViewStub,
+                presenter, view, activity))
+                .isEqualTo(NO_WARNINGS);
+    }
+
+    public void testTiActivity_andViewIsImplementedCorrectly_noWarnings() throws Exception {
+        TestFile activity = java("" +
                 "package foo;" +
                 "import net.grandcentrix.thirtyinch.*;" +
-                "final class MyPresenter extends TiPresenter<MyView> {" +
+                "public class MyActivity extends TiActivity<MyPresenter, MyView> implements MyView {" +
                 "}");
 
+        assertThat(lintProject(
+                tiActivityStub, tiPresenterStub, tiViewStub,
+                presenter, view, activity))
+                .isEqualTo(NO_WARNINGS);
+    }
+
+    public void testTiActivity_doesntImplementInterface_hasWarning() throws Exception {
         TestFile activity = java("" +
                 "package foo;" +
                 "import net.grandcentrix.thirtyinch.*;" +
                 "public class MyActivity extends TiActivity<MyPresenter, MyView> {" +
-                "   @Override" +
-                "   public MyPresenter providePresenter() {" +
-                "       return new MyPresenter();" +
+                "}");
+
+        assertThat(lintProject(
+                tiActivityStub, tiPresenterStub, tiViewStub,
+                presenter, view, activity))
+                .containsOnlyOnce(MissingTiViewImplementationDetector.ISSUE.getId());
+    }
+
+    public void testTiActivity_doesntImplementInterface_butOverridesProvideView_noWarnings() throws Exception {
+        TestFile activity = java("" +
+                "package foo;" +
+                "import net.grandcentrix.thirtyinch.*;" +
+                "public class MyActivity extends TiActivity<MyPresenter, MyView> {" +
+                "   public MyView provideView() {" +
+                "       return null;" +
                 "   }" +
                 "}");
 
         assertThat(lintProject(
                 tiActivityStub, tiPresenterStub, tiViewStub,
-                activity, presenter, view)).isEqualTo(NO_WARNINGS);
+                presenter, view, activity))
+                .isEqualTo(NO_WARNINGS);
     }
+
+    // TODO Ignored: For some reason, Lint doesn't resolve the base class
+    public void _testTiActivity_throughTransitiveBaseClass_hasWarning() throws Exception {
+        TestFile baseActivity = java("" +
+                "package foo;" +
+                "import net.grandcentrix.thirtyinch.*;" +
+                "public abstract class BaseActivity<P extends TiPresenter<V>, V extends TiView> extends TiActivity<P, V> {" +
+                "}");
+
+        TestFile activity = java("" +
+                "package foo;" +
+                "import net.grandcentrix.thirtyinch.*;" +
+                "public class MyActivity extends BaseActivity<MyPresenter, MyView> {" +
+                "}");
+
+        assertThat(lintProject(
+                tiActivityStub, tiPresenterStub, tiViewStub,
+                presenter, view, baseActivity, activity))
+                .containsOnlyOnce(MissingTiViewImplementationDetector.ISSUE.getId());
+    }
+
+    public void testTiFragment_dontTriggerOnAbstractClass() throws Exception {
+        TestFile fragment = java("" +
+                "package foo;" +
+                "import net.grandcentrix.thirtyinch.*;" +
+                "public abstract class MyFragment extends TiFragment<MyPresenter, MyView> {" +
+                "}");
+
+        assertThat(lintProject(
+                tiFragmentStub, tiPresenterStub, tiViewStub,
+                presenter, view, fragment))
+                .isEqualTo(NO_WARNINGS);
+    }
+
+    public void testTiFragment_andViewIsImplementedCorrectly_noWarnings() throws Exception {
+        TestFile fragment = java("" +
+                "package foo;" +
+                "import net.grandcentrix.thirtyinch.*;" +
+                "public class MyFragment extends TiFragment<MyPresenter, MyView> implements MyView {" +
+                "}");
+
+        assertThat(lintProject(
+                tiFragmentStub, tiPresenterStub, tiViewStub,
+                presenter, view, fragment))
+                .isEqualTo(NO_WARNINGS);
+    }
+
+    public void testTiFragment_doesntImplementInterface_hasWarning() throws Exception {
+        TestFile fragment = java("" +
+                "package foo;" +
+                "import net.grandcentrix.thirtyinch.*;" +
+                "public class MyFragment extends TiFragment<MyPresenter, MyView> {" +
+                "}");
+
+        assertThat(lintProject(
+                tiFragmentStub, tiPresenterStub, tiViewStub,
+                presenter, view, fragment))
+                .containsOnlyOnce(MissingTiViewImplementationDetector.ISSUE.getId());
+    }
+
+    public void testTiFragment_doesntImplementInterface_butOverridesProvideView_noWarnings() throws Exception {
+        TestFile fragment = java("" +
+                "package foo;" +
+                "import net.grandcentrix.thirtyinch.*;" +
+                "public class MyFragment extends TiFragment<MyPresenter, MyView> {" +
+                "   public MyView provideView() {" +
+                "       return null;" +
+                "   }" +
+                "}");
+
+        assertThat(lintProject(
+                tiFragmentStub, tiPresenterStub, tiViewStub,
+                presenter, view, fragment))
+                .isEqualTo(NO_WARNINGS);
+    }
+
+    // TODO Ignored: For some reason, Lint doesn't resolve the base class
+    public void _testTiFragment_throughTransitiveBaseClass_hasWarning() throws Exception {
+        TestFile baseFragment = java("" +
+                "package foo;" +
+                "import net.grandcentrix.thirtyinch.*;" +
+                "public abstract class BaseFragment<P extends TiPresenter<V>, V extends TiView> extends TiFragment<P, V> {" +
+                "}");
+
+        TestFile fragment = java("" +
+                "package foo;" +
+                "import net.grandcentrix.thirtyinch.*;" +
+                "public class MyFragment extends BaseFragment<MyPresenter, MyView> {" +
+                "}");
+
+        assertThat(lintProject(
+                tiFragmentStub, tiPresenterStub, tiViewStub,
+                presenter, view, baseFragment, fragment))
+                .containsOnlyOnce(MissingTiViewImplementationDetector.ISSUE.getId());
+    }
+
+    /* Overrides */
 
     @Override
     protected Detector getDetector() {
